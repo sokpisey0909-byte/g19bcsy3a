a914da5dbba2ab10e4a4695ce01fb302e7c5d580
sokpisey0909-byte <sokpisey0909@gmail.com>
2026-03-30 07:39:52 +0700

    ist

--- pages/user/list.php
+++ pages/user/list.php
@@ -54,3 +54,38 @@
     </table>
 
 </div>
+
+<script>
+
+    $(document).ready(function () {
+        $('.btn-danger').click(function (e){
+            e.preventDefault();
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed) Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                }).then((result) => {
+                    if (result.isConfirmed){
+                        window.location.href = $(this).attr('href');
+                    }
+                    
+                })
+
+            });
+
+            
+        });
+
+    });
+
+
+</script>
